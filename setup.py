a37d45a143305aa7ebaea7a09b04879caf77a45d
Johann <jgordillo@ciencias.unam.mx>
2020-05-20 17:58:29 -0500

    Eliminando long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+#with open("README.md", "r") as f:
+#    long_description = f.read()
 
 
 setup(
    name='rsa',
    version='1.0',
    description='Implementación del criptosistema RSA',
-   long_description=long_description,
-   long_description_content_type="text/markdown",
+   #long_description=long_description,
+   #long_description_content_type="text/markdown",
    url="https://github.com/JohannGordillo/RSA-Cryptosystem",
    license="MIT",
    author='Johann Gordillo',
