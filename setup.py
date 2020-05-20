f327f9498a40d80549336249b81934673205b22f
Johann <jgordillo@ciencias.unam.mx>
2020-05-20 17:18:35 -0500

    Agregando el archivo setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='rsa',
+   version='1.0',
+   description='Implementación del criptosistema RSA',
+   license="MIT",
+   author='Johann Gordillo',
+   author_email='jgordillo@ciencias.unam.mx',
+   packages=['rsa']
+)
