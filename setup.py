31e09c285986dd160a52791d971f797082fa3ce8
Johann <jgordillo@ciencias.unam.mx>
2020-05-20 17:26:58 -0500

    Agregando long_description y la url del repositorio

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 setup(
    name='rsa',
    version='1.0',
    description='Implementación del criptosistema RSA',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/JohannGordillo/RSA-Cryptosystem",
    license="MIT",
    author='Johann Gordillo',
    author_email='jgordillo@ciencias.unam.mx',
