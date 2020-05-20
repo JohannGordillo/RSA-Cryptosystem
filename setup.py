82ba285af138ace183e6c8d631df99eb081157eb
Johann <jgordillo@ciencias.unam.mx>
2020-05-20 18:11:42 -0500

    Eliminand comentarios innecesarios

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup
 
-#with open("README.md", "r") as f:
-#    long_description = f.read()
-
-
 setup(
    name='rsa',
    version='1.0',
    description='Implementación del criptosistema RSA',
-   #long_description=long_description,
-   #long_description_content_type="text/markdown",
    url="https://github.com/JohannGordillo/RSA-Cryptosystem",
    license="MIT",
    author='Johann Gordillo',
